2ca77b9c64a0eb36eaae528f540cc56f360384a8
Nezeeeeee <serezha_knyazev_92@mail.ru>
2026-03-16 11:48:49 +0300

    update

--- NotesApp.WinForms/Forms/NoteForm.Designer.cs
+++ NotesApp.WinForms/Forms/NoteForm.Designer.cs
@@ -31,7 +31,7 @@
             System.Windows.Forms.Label lblTags = new System.Windows.Forms.Label();
 
             // NoteForm - УВЕЛИЧИВАЕМ ШИРИНУ
-            this.Size = new System.Drawing.Size(820, 620);
+            this.Size = new System.Drawing.Size(900, 620); // Увеличено с 850 до 900
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
@@ -40,25 +40,27 @@
             // lblTitle
             lblTitle.Text = "Title:";
             lblTitle.Location = new System.Drawing.Point(20, 20);
-            lblTitle.Size = new System.Drawing.Size(45, 20);
+            lblTitle.Size = new System.Drawing.Size(100, 20); // Увеличено с 60 до 100
             lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10);
+            lblTitle.TextAlign = ContentAlignment.MiddleRight; // Выравнивание по правому краю
 
             // txtTitle
             this.txtTitle = new System.Windows.Forms.TextBox();
-            this.txtTitle.Location = new System.Drawing.Point(80, 18);
-            this.txtTitle.Size = new System.Drawing.Size(710, 25);
+            this.txtTitle.Location = new System.Drawing.Point(130, 18); // Сдвинуто с 90 на 130
+            this.txtTitle.Size = new System.Drawing.Size(740, 25); // Увеличено с 730 до 740
             this.txtTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10);
 
-            // lblContent
+            // lblContent - УВЕЛИЧИВАЕМ МЕТКУ ДЛЯ РУССКОГО ТЕКСТА
             lblContent.Text = "Content:";
             lblContent.Location = new System.Drawing.Point(20, 60);
-            lblContent.Size = new System.Drawing.Size(55, 20);
+            lblContent.Size = new System.Drawing.Size(100, 20); // Увеличено с 70 до 100
             lblContent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10);
+            lblContent.TextAlign = ContentAlignment.MiddleRight; // Выравнивание по правому краю
 
             // txtContent
             this.txtContent = new System.Windows.Forms.TextBox();
-            this.txtContent.Location = new System.Drawing.Point(80, 58);
-            this.txtContent.Size = new System.Drawing.Size(710, 350);
+            this.txtContent.Location = new System.Drawing.Point(130, 58); // Сдвинуто с 90 на 130
+            this.txtContent.Size = new System.Drawing.Size(740, 350); // Увеличено с 730 до 740
             this.txtContent.Multiline = true;
             this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.txtContent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10);
@@ -66,13 +68,14 @@
             // lblTags
             lblTags.Text = "Tags:";
             lblTags.Location = new System.Drawing.Point(20, 430);
-            lblTags.Size = new System.Drawing.Size(45, 20);
+            lblTags.Size = new System.Drawing.Size(100, 20); // Увеличено с 60 до 100
             lblTags.Font = new System.Drawing.Font("Microsoft Sans Serif", 10);
+            lblTags.TextAlign = ContentAlignment.MiddleRight;
 
             // flpTagsContainer - контейнер для тегов
             this.flpTagsContainer = new System.Windows.Forms.FlowLayoutPanel();
-            this.flpTagsContainer.Location = new System.Drawing.Point(80, 428);
-            this.flpTagsContainer.Size = new System.Drawing.Size(710, 100);
+            this.flpTagsContainer.Location = new System.Drawing.Point(130, 428); // Сдвинуто с 90 на 130
+            this.flpTagsContainer.Size = new System.Drawing.Size(740, 100); // Увеличено с 730 до 740
             this.flpTagsContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.flpTagsContainer.AutoScroll = true;
             this.flpTagsContainer.Padding = new System.Windows.Forms.Padding(8);
@@ -81,8 +84,8 @@
             // btnSave
             this.btnSave = new System.Windows.Forms.Button();
             this.btnSave.Text = "Save";
-            this.btnSave.Location = new System.Drawing.Point(590, 545);
-            this.btnSave.Size = new System.Drawing.Size(95, 35);
+            this.btnSave.Location = new System.Drawing.Point(640, 545); // Сдвинуто с 610 на 640
+            this.btnSave.Size = new System.Drawing.Size(110, 35); // Увеличено с 100 до 110
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10);
             this.btnSave.BackColor = System.Drawing.Color.LightSteelBlue;
             this.btnSave.FlatStyle = FlatStyle.Flat;
@@ -92,8 +95,8 @@
             // btnCancel
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnCancel.Text = "Cancel";
-            this.btnCancel.Location = new System.Drawing.Point(695, 545);
-            this.btnCancel.Size = new System.Drawing.Size(95, 35);
+            this.btnCancel.Location = new System.Drawing.Point(760, 545); // Сдвинуто с 720 на 760
+            this.btnCancel.Size = new System.Drawing.Size(110, 35); // Увеличено с 100 до 110
             this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10);
             this.btnCancel.BackColor = System.Drawing.Color.LightGray;
             this.btnCancel.FlatStyle = FlatStyle.Flat;
